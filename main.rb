756e4dce47b33affa8b7ce80deb7d1f15aca5ed9
gwenmorgana <gwenmorgana@gmail.com>
2018-11-30 21:34:09 -0800

    more studies

--- main.rb
+++ main.rb
@@ -1,3 +1,19 @@
+
+
+* Map
+  - next_scene
+  - opening_scene
+* Engine
+  - play
+* Scene
+  - enter
+  * Death
+  * Central Corridor
+  * Laser Weapon Armory
+  * The Bridge
+  * Escape Pod
+
+
 class Scene
   def enter()
   end
@@ -15,7 +31,16 @@ end
 
 class Death < Scene
 
+  @@quips = [
+    "You died.  You kinda suck at this.",
+     "Your mom would be proud...if she were smarter.",
+     "Such a luser.",
+     "I have a small puppy that's better at this."
+  ]
+
   def enter()
+    puts @@quips[rand(0..(@@quips.length - 1))]
+    exit(1)
   end
 end
 
